aad2c8c14e6dd78e371e346f3a3d5f7b63367116
Daniyar_Akhmet <daniyar@asitek.kz>
2020-03-10 11:38:54 +0600

    Простое приложение на Flask

---
+++ webapp/python_org_news.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_html(url) -> 'html':
+    try:
+        result = requests.get(url)
+        result.raise_for_status()
+        return result.text
+    except(requests.RequestException, ValueError):
+        print('Сетевая ошибка')
+        return False
+
+def get_python_news() -> 'list_dict':
+    html = get_html('https://www.python.org/blogs/')
+    if html:
+        soup = BeautifulSoup(html, 'html.parser') #преобразуем полученный html в дерево soup
+        all_news = soup.find('ul', class_='list-recent-posts').findAll('li')
+        result_news = []
+        foor news in all_news:
+            title = news.find('a').text
+            url = news.find('a')['href']
+            published = news.find('time').text
+            result_news.append({
+                'title': title,
+                'url': url,
+                'published': published
+            })
+        return result_news
+    return False
